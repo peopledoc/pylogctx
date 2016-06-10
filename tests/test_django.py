9f9afc673917cc3632ab1380d0ee8bf77698d251
Étienne BERSAC <etienne.bersac@people-doc.com>
2016-06-10 16:54:20 +0200

    Fix extractor middleware tests
    Fixes:
    
        @patch('pylogctx.django.settings',
               PYLOGCTX_REQUEST_EXTRACTOR=_failing_extractor)
        def test_middleware_extraction_failed(request):
            with patch('pylogctx.django.logger') as m:
                ExtractRequestContextMiddleware().process_request(request)
    >           assert m.method_calls == [call.exception()]
    E           assert [call.debug('...l.exception()] == [call.exception()]
    E             At index 0 diff: call.debug('Failed to push request object to log context: %r', Exception("Can't log object of type <class 'mock.mock.MagicMock'>",)) != call.exception()
    E             Left contains more items, first extra item: call.exception()
    E             Use -v to get the full diff

--- tests/test_django.py
+++ tests/test_django.py
@@ -18,7 +18,7 @@ def request():
 @pytest.yield_fixture
 def context():
     log_context.update(rid=42)
-    yield None
+    yield log_context
     try:
         del log_context._stack
     except AttributeError:
@@ -40,14 +40,14 @@ def test_middleware_no_extractor(request):
 
 @patch('pylogctx.django.settings',
        PYLOGCTX_REQUEST_EXTRACTOR=_failing_extractor)
-def test_middleware_extraction_failed(request):
+def test_middleware_extraction_failed(settings, request):
     with patch('pylogctx.django.logger') as m:
         ExtractRequestContextMiddleware().process_request(request)
-        assert m.method_calls == [call.exception()]
+        assert call.exception() in m.method_calls
 
 
 @patch('pylogctx.django.settings', PYLOGCTX_REQUEST_EXTRACTOR=_extractor)
-def test_middleware_context_extracted(request, context):
+def test_middleware_context_extracted(settings, request, context):
     ExtractRequestContextMiddleware().process_request(request)
     fields = log_context.as_dict()
     assert 'rid' in fields
