baa41788c69e3e3360e28cc4ddfe64b4eedd31d6
Étienne BERSAC <etienne.bersac@people-doc.com>
2016-06-10 16:17:13 +0200

    Align task middleware to request one
    It's more consistent this way. Also, this avoid forcing celeryTask and
    celeryTaskId key. Some may prefer snake_case.

--- src/pylogctx/celery.py
+++ src/pylogctx/celery.py
@@ -1,18 +1,25 @@
 from __future__ import absolute_import
 
+import logging
+
 from celery.app import current_task
 from celery.app.task import Task
 
 from .core import context
 
 
+logger = logging.getLogger(__name__)
+
+
 class LoggingTask(Task):
     def __call__(self, *args, **kwargs):
         task = current_task()
-        context.update(
-            celeryTask=task.name,
-            celeryTaskId=task.request.id,
-        )
+
+        try:
+            context.update(task)
+        except Exception as e:
+            logger.debug('Failed to push task to log context: %r', e)
+
         try:
             return super(LoggingTask, self).__call__(*args, **kwargs)
         finally:
